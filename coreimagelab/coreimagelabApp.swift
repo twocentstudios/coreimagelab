6b7858c6a0004fcdd2e369d0738c30bb190d5cac
Christopher Trott <chris@twocentstudios.com>
2025-02-23 20:49:06 +0900

    Initial Commit

---
+++ coreimagelab/coreimagelabApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreimagelabApp.swift
+//  coreimagelab
+//
+//  Created by Christopher Trott on 2025/02/23.
+//
+
+import SwiftUI
+
+@main
+struct coreimagelabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
